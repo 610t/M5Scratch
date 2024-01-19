64901b319311a32db5d4e3ba879ad0b6c5fc6d98
Takeshi MUTOH <takeshi.mutoh@gmail.com>
2024-01-19 11:52:07 +0900

    Rearrange comments. - Change licens e to project top reference. - Update license file. - Add comments on fixed settings.

--- M5Scratch/M5Scratch.ino
+++ M5Scratch/M5Scratch.ino
@@ -1,42 +1,17 @@
 /*
-    Scratch remote sensor protocol
+  Scratch remote sensor protocol demonstration
 
-    You need to get ssid , password and host
+  This program is demonstration that Scrath Remote Sensor Protocol for M5Stack family.
 
-    http://tiisai.dip.jp/?p=3665
-*/
-
-/*
-  Copyright 2016,2019-2024 Takeshi MUTOH
-
-  Redistribution and use in source and binary forms, with or without modification,
-  are permitted provided that the following conditions are met:
-
-  1. Redistributions of source code must retain the above copyright notice,
-     this list of conditions and the following disclaimer.
-
-  2. Redistributions in binary form must reproduce the above copyright notice,
-     this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
-/*
-   This program is demonstration that Scrath Remote Sensor Protocol with M5Stack.
+  This software is provided at BSD 2-Clause "Simplified" License.
+  Please see more detail: https://github.com/610t/M5Scratch/blob/master/LICENSE .
+  Original work is at http://tiisai.dip.jp/?p=3665 .
 */
 
 //// Network settings
 
-// If you use fixed network settings below, use #undef WIFI_MODE_PREV
+// If you use fixed network settings below, use #undef WIFI_MODE_PREV and set ssid, password, and host.
+//  This setting is useful for no SD machine such like Atom series, M5Stick series and M5Dial.
 #define WIFI_MODE_PREV  // Use privious settings to setting up Wi-Fi.
 // #undef WIFI_MODE_PREV
 
